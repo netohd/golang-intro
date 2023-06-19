bc2c8614d21052bd20234cc77ce28a59556b9897
netohd <toninetocontato@gmail.com>
2023-06-19 19:14:46 -0300

    func definitions

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+func sum(x int, y int) (int, bool) {
+	return x + y, true
+}
+
 func main() {
-	println("Hello World!")
+	// O : declara a variavel, usa somente na 1a vez
+	name := "Antonio" 
+	name = "Pedro"
+	println(name)
+	println(sum(2, 5))
+
+	newSum()
+}
+
+func newSum() {
+	result, status := sum(1, 5)
+	println(result, status)
 }
